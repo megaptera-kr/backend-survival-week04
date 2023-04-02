ed9dcff39589e18ab565fa693f54840e0eea5da7
jack <tjdgns526@gmail.com>
2023-04-02 21:44:36 +0300

    [구성훈] 4주차 과제 - Layered Architecture 를 적용한 게시판 REST API

--- src/main/java/kr/megaptera/assignment/repositories/PostRepository.java
+++ src/main/java/kr/megaptera/assignment/repositories/PostRepository.java
@@ -1,4 +1,35 @@
 package kr.megaptera.assignment.repositories;
 
+import kr.megaptera.assignment.domains.Post;
+import kr.megaptera.assignment.domains.PostId;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class PostRepository {
+    Map<PostId, Post> posts;
+
+    public PostRepository() {
+        this.posts = new HashMap<>();
+    }
+
+    public List<Post> findAll() {
+        return new ArrayList<>(posts.values());
+    }
+
+    public Post find(PostId id) {
+        Post post = posts.get(id);
+
+        return post;
+    }
+
+    public void save(Post post) {
+        posts.put(post.id(), post);
+    }
+
+    public void delete(PostId id) {
+        posts.remove(id);
+    }
 }
