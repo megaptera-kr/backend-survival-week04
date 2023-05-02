7b8d8eb60a3454d67775b8d34d6b7ee3783ad9b9
spacekay <bethjkim0311@gmail.com>
2023-05-03 00:47:53 +0900

    feat: Post 관련 기능 구현

--- src/main/java/kr/megaptera/assignment/controllers/PostController.java
+++ src/main/java/kr/megaptera/assignment/controllers/PostController.java
@@ -1,4 +1,79 @@
 package kr.megaptera.assignment.controllers;
 
+import kr.megaptera.assignment.application.PostService;
+import kr.megaptera.assignment.exceptions.PostNotFound;
+import kr.megaptera.assignment.models.dtos.PostCreateDto;
+import kr.megaptera.assignment.models.dtos.PostDto;
+import kr.megaptera.assignment.models.dtos.PostUpdateDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/posts")
+@RestController
 public class PostController {
+
+    private final PostService postService;
+
+    // 생성자
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    // 전체 게시물 리스트 가져오기
+    @GetMapping
+    public List<PostDto> list() {
+        return postService.getPosts();
+    }
+
+    // 특정 게시물 가져오기
+    @GetMapping("/{id}")
+    public PostDto details(
+            @PathVariable String id
+    ) {
+        return postService.getPost(id);
+    }
+
+    // 게시물 새로 작성하기
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public PostDto create(
+            @RequestBody PostCreateDto postCreateDto) {
+        return postService.createPost(postCreateDto);
+    }
+
+    // 게시물 수정하기
+    @PatchMapping("/{id}")
+    public PostDto update(
+            @PathVariable String id,
+            @RequestBody PostUpdateDto postUpdateDto
+    ) {
+        return postService.updatePost(id, postUpdateDto);
+    }
+
+    // 게시물 삭제하기
+    @DeleteMapping("/{id}")
+    public PostDto delete(
+            @PathVariable String id
+    ) {
+        return postService.deletePost(id);
+    }
+
+    // 원하는 글이 없는 경우 404 (빈 리스트 또는 404로 return 되도록)
+    @ExceptionHandler(PostNotFound.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String postNotFound() {
+        return "There's no post you want to see.\n";
+    }
+
 }
--- src/main/java/kr/megaptera/assignment/repositories/PostRepository.java
+++ src/main/java/kr/megaptera/assignment/repositories/PostRepository.java
@@ -1,4 +1,47 @@
 package kr.megaptera.assignment.repositories;
 
+import kr.megaptera.assignment.exceptions.PostNotFound;
+import kr.megaptera.assignment.models.Post;
+import kr.megaptera.assignment.models.PostId;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
 public class PostRepository {
+
+    private final Map<PostId, Post> posts;
+
+    // 생성자 (이번 주차 과제에서는 외부 주입 없이 내부에서 생성하도록 함)
+    public PostRepository() {
+        // PostId class는 Hashable하므로 HashMap의 key object로 정의 가능
+        this.posts = new HashMap<>();
+    }
+
+    // 전체 게시물 보기
+    public List<Post> findAll() {
+        return new ArrayList<>(posts.values());
+    }
+
+    // 특정 게시물 보기
+    public Post find(String id) {
+        Post post = posts.get(PostId.of(id));
+        if (post == null)
+            throw new PostNotFound();
+        return post;
+    }
+
+    // 게시물 저장하기
+    public Post save(Post post) {
+        posts.put(post.id(), post);
+        return post;
+    }
+
+    // 게시물 삭제하기
+    public Post delete(String id) {
+        return posts.remove(PostId.of(id));
+    }
 }
