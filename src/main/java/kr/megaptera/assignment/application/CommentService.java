1333928b451b366d1451096f25a5b5093e075062
spacekay <bethjkim0311@gmail.com>
2023-05-03 00:48:02 +0900

    feat: Comment 관련 기능 구현

--- src/main/java/kr/megaptera/assignment/application/CommentService.java
+++ src/main/java/kr/megaptera/assignment/application/CommentService.java
@@ -1,4 +1,46 @@
 package kr.megaptera.assignment.application;
 
+import kr.megaptera.assignment.models.Comment;
+import kr.megaptera.assignment.models.PostId;
+import kr.megaptera.assignment.models.dtos.CommentCreateDto;
+import kr.megaptera.assignment.models.dtos.CommentDto;
+import kr.megaptera.assignment.models.dtos.CommentUpdateDto;
+import kr.megaptera.assignment.repositories.CommentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    // 생성자
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    // 해당 게시물의 전체 댓글 리스트 가져오기
+    public List<CommentDto> getComments(String postId) {
+        return commentRepository.findAllByPost(postId)
+                .stream().map(CommentDto::new).toList();
+    }
+
+    // 댓글 새로 작성하기
+    public CommentDto createComment(String postId, CommentCreateDto commentCreateDto) {
+        Comment comment = new Comment(PostId.of(postId), commentCreateDto);
+        return new CommentDto(commentRepository.save(postId, comment));
+    }
+
+    // 댓글 수정하기
+    public CommentDto updateComment(String id, String postId, CommentUpdateDto commentUpdateDto) {
+        Comment comment = commentRepository.find(id, postId);
+        comment.updateComment(commentUpdateDto);
+        return new CommentDto(commentRepository.save(postId, comment));
+    }
+
+    // 댓글 삭제하기
+    public CommentDto deleteComment(String id, String postId) {
+        return new CommentDto(commentRepository.delete(id, postId));
+    }
 }
--- src/main/java/kr/megaptera/assignment/repositories/CommentRepository.java
+++ src/main/java/kr/megaptera/assignment/repositories/CommentRepository.java
@@ -1,4 +1,57 @@
 package kr.megaptera.assignment.repositories;
 
+import kr.megaptera.assignment.exceptions.CommentNotFound;
+import kr.megaptera.assignment.models.Comment;
+import kr.megaptera.assignment.models.CommentId;
+import kr.megaptera.assignment.models.PostId;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
 public class CommentRepository {
+
+    private final Map<PostId, Map<CommentId, Comment>> comments;
+
+    // 생성자 (이번 주차 과제에서는 외부 주입 없이 내부에서 생성하도록 함)
+    public CommentRepository() {
+        // PostId, CommentId class는 Hashable하므로 HashMap의 key object로 정의 가능
+        this.comments = new HashMap<>();
+    }
+
+    // 댓글 찾기
+    public Comment find(String id, String postId) {
+        Map<CommentId, Comment> commentsOnPost = comments.get(PostId.of(postId));
+        if (commentsOnPost == null)
+            throw new CommentNotFound();
+
+        return commentsOnPost.get(CommentId.of(id));
+    }
+
+    // 해당 게시물의 전체 댓글 리스트 찾기
+    public List<Comment> findAllByPost(String postId) {
+        Map<CommentId, Comment> commentsOnPost = comments.get(PostId.of(postId));
+        if (commentsOnPost == null)
+            commentsOnPost = new HashMap<>();
+        return commentsOnPost.values().stream().toList();
+    }
+
+    // 댓글 저장하기
+    public Comment save(String postId, Comment comment) {
+        Map<CommentId, Comment> commentsOnPost = comments.get(PostId.of(postId));
+        if (commentsOnPost == null)
+            commentsOnPost = new HashMap<>();
+        commentsOnPost.put(comment.id(), comment);
+        comments.put(PostId.of(postId), commentsOnPost);
+        return comment;
+    }
+
+    // 댓글 삭제하기
+    public Comment delete(String id, String postId) {
+        Comment comment = find(id, postId);
+        comments.get(PostId.of(postId)).remove(CommentId.of(id));
+        return comment;
+    }
 }
