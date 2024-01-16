c1fc91608ddc5f8fb2909af7005ad4cef95ca96e
parkmyungkwan <pmk3762@gmail.com>
2024-01-16 09:10:21 +0900

    4주차 과제 commit

--- src/main/java/kr/megaptera/assignment/controllers/CommentController.java
+++ src/main/java/kr/megaptera/assignment/controllers/CommentController.java
@@ -1,4 +1,66 @@
 package kr.megaptera.assignment.controllers;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.megaptera.assignment.application.CommentService;
+import kr.megaptera.assignment.dtos.CommentCreateDto;
+import kr.megaptera.assignment.dtos.CommentDto;
+import kr.megaptera.assignment.dtos.CommentUpdatedDto;
+import kr.megaptera.assignment.exceptions.CommentNotFound;
+
+@RestController
+@RequestMapping("/comments")
+@CrossOrigin
 public class CommentController {
+	private final CommentService commentService;
+
+	public CommentController() {
+		this.commentService = new CommentService();
+	}
+
+	@GetMapping
+	public List<CommentDto> list(@RequestParam String postId) {
+		List<CommentDto> commentDtos = commentService.getCommentDtos(postId);
+		return commentDtos;
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public CommentDto create(@RequestParam String postId, @RequestBody CommentCreateDto commentCreateDto) {
+		CommentDto created = commentService.createComment(postId, commentCreateDto);
+		return created;
+	}
+
+	@PatchMapping("/{id}")
+	public CommentDto update(@PathVariable String id, @RequestParam String postId,
+		@RequestBody CommentUpdatedDto commentUpdatedDto) {
+		CommentDto updated = commentService.updateComment(id, postId, commentUpdatedDto);
+		return updated;
+	}
+
+	@DeleteMapping("/{id}")
+	public CommentDto update(@PathVariable String id, @RequestParam String postId) {
+		CommentDto deleted = commentService.deleteComment(id, postId);
+		return deleted;
+	}
+
+	@ExceptionHandler(CommentNotFound.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public String postNotFound() {
+		return "댓글을 찾을 수 없습니다.";
+	}
 }
--- src/main/java/kr/megaptera/assignment/controllers/PostController.java
+++ src/main/java/kr/megaptera/assignment/controllers/PostController.java
@@ -1,4 +1,72 @@
 package kr.megaptera.assignment.controllers;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.megaptera.assignment.application.PostService;
+import kr.megaptera.assignment.dtos.PostCreateDto;
+import kr.megaptera.assignment.dtos.PostDto;
+import kr.megaptera.assignment.dtos.PostUpdateDto;
+import kr.megaptera.assignment.exceptions.PostNotFound;
+
+@RestController
+@RequestMapping("/posts")
+@CrossOrigin
 public class PostController {
+	private final PostService postService;
+
+	public PostController() {
+		this.postService = new PostService();
+	}
+
+	@GetMapping
+	public List<PostDto> list() {
+		List<PostDto> postDtos = postService.getPostDtos();
+		return postDtos;
+	}
+
+	@GetMapping("/{id}")
+	public PostDto detail(@PathVariable String id) {
+		PostDto postDto = postService.getPostDto(id);
+
+		return postDto;
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public PostDto create(@RequestBody PostCreateDto postCreateDto) {
+		PostDto created = postService.createPost(postCreateDto);
+		return created;
+	}
+
+	@PatchMapping("/{id}")
+	public PostDto update(@PathVariable String id,
+		@RequestBody PostUpdateDto postUpdateDto) {
+		PostDto updated = postService.updatePost(id, postUpdateDto);
+		return updated;
+	}
+
+	@DeleteMapping("/{id}")
+	public PostDto delete(@PathVariable String id) {
+		PostDto postDto = postService.deletePost(id);
+		return postDto;
+	}
+
+	@ExceptionHandler(PostNotFound.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public String postNotFound() {
+		return "게시물을 찾을 수 없습니다.";
+	}
 }
