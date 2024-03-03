f69b995a07f8ea335e9b82aecd49b53a47392e6b
Jeongpope <nullptr-@kakao.com>
2024-03-03 14:15:16 +0900

    4 weeks

--- src/main/java/kr/megaptera/assignment/controllers/CommentController.java
+++ src/main/java/kr/megaptera/assignment/controllers/CommentController.java
@@ -1,4 +1,57 @@
 package kr.megaptera.assignment.controllers;
 
+import kr.megaptera.assignment.application.CommentService;
+import kr.megaptera.assignment.dtos.CommentCreateDto;
+import kr.megaptera.assignment.dtos.CommentDto;
+import kr.megaptera.assignment.dtos.CommentUpdateDto;
+import kr.megaptera.assignment.exceptions.CommentNotFound;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/comments")
+@CrossOrigin
 public class CommentController {
+    private final CommentService commentService;
+
+    public CommentController() {
+        this.commentService = new CommentService();
+    }
+
+    @GetMapping
+    public List<CommentDto> list(@RequestParam String postId) {
+        return commentService.getCommentDots(postId);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CommentDto create(@RequestParam String postId,
+                             @RequestBody CommentCreateDto commentCreateDto) {
+        return commentService
+                .createComment(postId, commentCreateDto);
+    }
+
+    @PatchMapping("/{id}")
+    public CommentDto update(
+            @PathVariable String id,
+            @RequestParam String postId,
+            @RequestBody CommentUpdateDto commentUpdatedDto
+    ) {
+        return commentService
+                .updateComment(id, postId, commentUpdatedDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public CommentDto delete(
+            @PathVariable String id,
+            @RequestParam String postId
+    ) {
+        return commentService.deleteComment(id, postId);
+    }
+
+    @ExceptionHandler(CommentNotFound.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String postNotFound() { return "Not found comments."; }
 }
--- src/main/java/kr/megaptera/assignment/repositories/PostRepository.java
+++ src/main/java/kr/megaptera/assignment/repositories/PostRepository.java
@@ -1,4 +1,40 @@
 package kr.megaptera.assignment.repositories;
 
+import kr.megaptera.assignment.exceptions.PostNotFound;
+import kr.megaptera.assignment.models.Post;
+import kr.megaptera.assignment.models.PostId;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class PostRepository {
+    Map<PostId, Post> posts;
+
+    public PostRepository() {
+        this.posts = new HashMap<>();
+    }
+
+    public List<Post> findAll() {
+        return new ArrayList<>(posts.values());
+    }
+
+    public Post find(PostId id) {
+        Post post = posts.get(id);
+
+        if (post == null) {
+            throw new PostNotFound();
+        }
+
+        return post;
+    }
+
+    public void save(Post post) {
+        posts.put(post.id(), post);
+    }
+
+    public void delete(PostId id) {
+        posts.remove(id);
+    }
 }
